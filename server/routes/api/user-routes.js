03f9599049116b1474587d802440911cfb837ed5
Hank Star <kate.stapley@hotmail.com>
2024-10-27 09:28:12 +1100

    index.js missing from schemas folder. pathways in files corrected.

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -5,7 +5,7 @@ const {
   saveBook,
   deleteBook,
   login,
-} = require('../../controllers/user-controller');
+} = require('../../config/controllers/user-controller');
 
 // import middleware
 const { authMiddleware } = require('../../utils/auth');
---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+// Import your type definitions and resolvers
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+// Export them together
+module.exports = { typeDefs, resolvers };
